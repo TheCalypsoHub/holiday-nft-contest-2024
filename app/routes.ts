f837a9621795c7669d4d0e29eaaf2c23c238eece
TheGreatAxios <sawyer@dirtroad.dev>
2024-12-24 13:44:32 -0800

    Remove Navigation Items

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("/mint", "routes/mint.tsx"),
-    route("/vote", "routes/vote.tsx"),
-    route("/api/image/:id", "routes/api/image.tsx")
+    // route("/mint", "routes/mint.tsx"),
+    // route("/vote", "routes/vote.tsx"),
+    // route("/api/image/:id", "routes/api/image.tsx")
 ] satisfies RouteConfig;
