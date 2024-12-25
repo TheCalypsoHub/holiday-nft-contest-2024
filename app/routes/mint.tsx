12bce4311fe79a0d74317cc3f1d7e414b7635c50
TheGreatAxios <sawyer@dirtroad.dev>
2024-12-25 07:33:02 -0800

    Make Button Cleaner

--- app/routes/mint.tsx
+++ app/routes/mint.tsx
@@ -153,7 +153,7 @@ export default function Mint() {
                                 type="submit"
                                 disabled={fetcher.state === "submitting"}
                             >
-                                Mint NFT
+                                Upload Image &amp; Mint NFT
                             </button>
                         </fetcher.Form>
                     </div>
